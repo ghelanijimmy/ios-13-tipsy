8f01940d441643afc7684b3ebb17979261bae4e5
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2020-07-11 08:17:00 -0400

    Completed project

---
+++ Tipsy/Controllers/CalculateViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip = 0.1
+
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        tip = Double(String(sender.currentTitle!.dropLast()))! / 100
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: Any) {
+        print(splitNumberLabel.text!)
+    }
+}
+
